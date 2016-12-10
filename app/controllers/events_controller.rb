1af13dc1fd7a38e29371ed0a9830f5b033fc48fe
tenra <11tenra@gmail.com>
2016-12-10 14:45:41 +0000

    event_edit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,24 @@
 class EventsController < ApplicationController
+    before_action :sign_in_required, only: [:edit, :update]
+
+  
   def show
       @event = Event.find(params[:id])
-
+  end
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    @event.assign_attributes(event_params)
+    if @event.save
+      flash[:success] = "update completed!"
+      redirect_to @event
+    else
+      render 'edit'
+    end
   end
   
 private
