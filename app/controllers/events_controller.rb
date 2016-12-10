60bfdb304ef9dc7c1b3c67af4765c4ada5af02ae
tenra <11tenra@gmail.com>
2016-12-10 13:16:41 +0000

    event_show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,14 @@
 class EventsController < ApplicationController
   def show
-          @user = User.find(params[:id])
+      @event = Event.find(params[:id])
 
   end
+  
+private
+    def event_params
+      params.require(:event).permit(
+      :title, :text
+        )
+    end
+  
 end
