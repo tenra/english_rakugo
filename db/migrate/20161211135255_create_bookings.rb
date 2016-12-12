06102e91d3acdee2d9f18b9cf95c5108ebe2e8ff
tenra <11tenra@gmail.com>
2016-12-12 12:46:12 +0000

    drop down manu inline

--- db/migrate/20161211135255_create_bookings.rb
+++ db/migrate/20161211135255_create_bookings.rb
@@ -3,7 +3,6 @@ class CreateBookings < ActiveRecord::Migration
     create_table :bookings do |t|
       t.references :user, index: true, foreign_key: true
       t.references :event, index: true, foreign_key: true
-      t.string :type
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209115807) do
+ActiveRecord::Schema.define(version: 20161211135255) do
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "bookings", ["event_id"], name: "index_bookings_on_event_id"
+  add_index "bookings", ["user_id"], name: "index_bookings_on_user_id"
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
