d48af56c82b3d1eaf2f3444c01db6cd41135c9e7
tenra <11tenra@gmail.com>
2016-12-20 11:33:11 +0000

    default_avatar_previewing OK

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -26,12 +26,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    "no-avatar.png"
+  end
 
    process :resize_to_limit => [700, 700]
    
