8cb8475a0f3b2906bacd4fbb02a46288a1ccaf8f
tenra <11tenra@gmail.com>
2017-02-16 06:01:55 +0000

    field_with_errors

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,9 @@ module EnglishRakugo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }  #field_with_errors対策
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
