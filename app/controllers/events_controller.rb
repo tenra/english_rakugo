d9fd4535d5ef89773a2a59d8b8de4c7873a93cb7
tenra <11tenra@gmail.com>
2017-02-19 05:10:11 +0000

    back

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -52,7 +52,7 @@ class EventsController < ApplicationController
 
   def confirm2
     @event = Event.find(params[:id])
-    @event.assign_attributes(event_params) if !Rails.env.production?
+    @event.assign_attributes(event_params)
     render :edit if @event.invalid?
     #binding.pry
   end
