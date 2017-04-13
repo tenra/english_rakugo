2d77f270f7b988539aed90301a713382f7ed5c4a
tenra <11tenra@gmail.com>
2017-04-13 12:04:34 +0000

    introductionsController new,create

--- app/controllers/introductions_controller.rb
+++ app/controllers/introductions_controller.rb
@@ -1,2 +1,29 @@
 class IntroductionsController < ApplicationController
+    before_action :sign_in_required
+    before_action :organizer_check
+    before_action :already_build, only: [:new]
+    
+    def new
+        @introduction = Introduction.new
+    end
+    
+    def create
+        @introduction = Introduction.new(introduction_params)
+        @introduction.user = current_user
+        if @introduction.save
+            flash[:success] = "Created your Introduction Photo!"
+            redirect_to organizer_url
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    def profile_params
+        params.require(:introduction).permit(:photo, :photo_cache, :name, :sentence)
+    end
+    
+    def already_build
+        redirect_to edit_introduction_path(current_user.introduction) if current_user.introduction
+    end        
 end
